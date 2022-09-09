d416d6fc407df8bd176d69ed7985a9ca9229b07d
Emmanuel <nkememmanuel5505@gmail.com>
2022-09-09 14:01:38 -0700

    Task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,6 +8,7 @@ int main(void)
 {
 
 	int i, j, k;
+
 	for (i = 48; i < 58; i++)
 	{
 		for (j = i; j < 58; j++)
@@ -31,6 +32,7 @@ int main(void)
 					putchar(',');
 					putchar(' ');
 				}
+
 			}
 		}
 	}
